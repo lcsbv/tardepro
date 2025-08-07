7d54362cfaa9dafa393dda6ae3d938b0525358b1
lcsbv <lucas03bezerra@gmail.com>
2025-08-07 01:33:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 function learnMore() {
-  // Código do Brasil é 55, então o número fica 5562998052045
-  window.open('https://wa.me/5562998052045?text=Olá!%20👋%20Tenho%20interesse%20em%20saber%20mais%20sobre%20o%20Robô%20Trade%20Pro%20que%20vi%20no%20seu%20site.%20🤖💹%20Pode%20me%20enviar%20mais%20informações,%20por%20favor?', '_blank');
+  window.open('https://wa.me/5562998052045?text=Olá!%20👋%20Achei%20interessante%20o%20Robô%20Trade%20Pro%20do%20seu%20site.%20Pode%20me%20explicar%20como%20funciona?', '_blank');
 }
