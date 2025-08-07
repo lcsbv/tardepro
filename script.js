d85198f79c13cfb2db2fa56c141636bf7a29b653
lcsbv <lucas03bezerra@gmail.com>
2025-08-07 14:11:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function learnMore() {
-  window.open('https://wa.me/5562998052045?text=Olá!%20👋%20Achei%20interessante%20o%20Robô%20Trade%20Pro%20do%20seu%20site.%20Pode%20me%20explicar%20como%20funciona?', '_blank');
+  window.open('https://go.hotmart.com/C101240954O?dp=1', '_blank');
 }
+
